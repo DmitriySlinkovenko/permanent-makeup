6aad3ab5959d6f46eefc722c473e8f1cf4adb35b
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-08-21 23:04:11 -0400

    Footer foundation complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,5 +2,15 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div>
+      <img src="" alt="" className="footer__logo" />
+      <p className="footer__text">
+        Made by{" "}
+        <a href="" className="footer__link">
+          Dmitriy Slinkovenko
+        </a>
+      </p>
+    </div>
+  );
 }
