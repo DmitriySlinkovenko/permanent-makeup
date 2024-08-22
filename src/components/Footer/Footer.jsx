d442af1a2ec1b083db5e26a398429c8135f0384d
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-08-22 19:18:07 -0400

    Footer foundation continue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,10 @@ export default function Footer() {
       <img src="" alt="" className="footer__logo" />
       <p className="footer__text">
         Made by{" "}
-        <a href="" className="footer__link">
+        <a
+          href="https://main.d1td2xkq4kk9nv.amplifyapp.com/"
+          className="footer__link"
+        >
           Dmitriy Slinkovenko
         </a>
       </p>
